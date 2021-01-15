0922bfaee2147d333ffb416435d56c128ac322f4
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2021-01-14 22:24:37 -0800

    defined Binary Search Tree Class To encapsulate all the functions of the binary search tree in one place.

--- BinarySearchTree/binarySearchTree_Implementation.cpp
+++ BinarySearchTree/binarySearchTree_Implementation.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
 #include <stdio.h>
-
+#include "binaryTreeClass.h"
 
 int main(){
-
     return 0;
 }
---
+++ BinarySearchTree/binaryTreeClass.h
@@ -0,0 +1,31 @@
+// Tree Node Structure
+
+struct tree_node{
+    int data;  // For node data
+    tree_node *left, *right; // Node pointers to hold left and right subtree
+};
+
+class BinarySearchTree{
+    private: 
+    tree_node *root; // Root node of the tree
+
+    // Binary Search Tree Methods
+    void insert(tree_node* node, int data); 
+    bool searhTree(tree_node* node, int data);
+
+    // Traversing tree
+    void pre_order(tree_node *root);
+    void post_order(tree_node *root);
+    void in_order(tree_node *root); 
+
+    public: 
+    
+    // The public methods will call the private methods AND handle the scenario when root == NULL (i.e. empty tree)
+
+    void insert(int data);
+    bool searchTree(int data);
+    void pre_order();
+    void post_order();
+    void in_order();
+};
+
